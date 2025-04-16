4c75dcb5ed880e2480138405dff35d3597a30dc7
TASTAN <54810017+tastangh@users.noreply.github.com>
2025-04-16 09:04:53 +0000

    update for release

--- detector/orb_hazmat_detector.py
+++ detector/orb_hazmat_detector.py
@@ -1,7 +1,66 @@
 import cv2
 import os
 import numpy as np
-from utils.iou_nms import non_max_suppression 
+
+def calculate_iou(boxA, boxB):
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[0] + boxA[2], boxB[0] + boxB[2])
+    yB = min(boxA[1] + boxA[3], boxB[1] + boxB[3])
+
+    interArea = max(0, xB - xA) * max(0, yB - yA)
+    if interArea == 0:
+        return 0.0
+
+    boxAArea = boxA[2] * boxA[3]
+    boxBArea = boxB[2] * boxB[3]
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
+
+def non_max_suppression(boxes_scores_labels, overlapThresh):
+    if len(boxes_scores_labels) == 0:
+        return []
+
+    boxes_scores_labels.sort(key=lambda x: x[2], reverse=True)
+
+    labels = [item[0] for item in boxes_scores_labels]
+    boxes = [item[1] for item in boxes_scores_labels]
+    scores = [item[2] for item in boxes_scores_labels]
+
+    np_boxes = np.array([[b[0], b[1], b[0] + b[2], b[1] + b[3]] for b in boxes])
+    pick_indices = [] 
+
+    x1 = np_boxes[:, 0]
+    y1 = np_boxes[:, 1]
+    x2 = np_boxes[:, 2]
+    y2 = np_boxes[:, 3]
+    area = (x2 - x1) * (y2 - y1) 
+
+    idxs = np.arange(len(scores))
+
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick_indices.append(i)
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        w = np.maximum(0, xx2 - xx1)
+        h = np.maximum(0, yy2 - yy1)
+
+        intersection = w * h
+        union = area[i] + area[idxs[:last]] - intersection
+        overlap = intersection / union
+
+        idxs_to_delete = np.concatenate(([last], np.where(overlap > overlapThresh)[0]))
+        idxs = np.delete(idxs, idxs_to_delete)
+
+    final_detections = [(labels[i], boxes[i]) for i in pick_indices]
+    return final_detections
 
 TEMPLATE_DIR = "./data/hazmats"
 
--- main.py
+++ main.py
@@ -12,6 +12,86 @@ HAZMAT_DETECTION_INTERVAL = 3  # Her N frame'de bir hazmat tespiti yapma
 IOU_THRESHOLD_BARREL_HAZMAT = 0.2  # Bir varil ile hazmat çakışıyorsa (IoU), varil iptal etme
 PAUSE_ON_NEW_DETECTION = True  # Her tespitte video durdurulacak
 
+def draw_detections(frame, detections):
+    for label, (x, y, w, h) in detections:
+        color = (0, 255, 0)
+        if "Varil" in label:
+            color = (255, 0, 0) if "Mavi" in label else (0, 0, 255) 
+        elif "Hazmat" in label or any(char.isdigit() for char in label): 
+             color = (0, 255, 255) 
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+        label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        y_label = max(y, label_size[1] + 10)
+        cv2.rectangle(frame, (x, y_label - label_size[1] - 10),
+                      (x + label_size[0], y_label + base_line - 10), color, cv2.FILLED)
+
+        cv2.putText(frame, label, (x, y_label - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+    return frame
+
+
+def calculate_iou(boxA, boxB):
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[0] + boxA[2], boxB[0] + boxB[2])
+    yB = min(boxA[1] + boxA[3], boxB[1] + boxB[3])
+
+    interArea = max(0, xB - xA) * max(0, yB - yA)
+    if interArea == 0:
+        return 0.0
+
+    boxAArea = boxA[2] * boxA[3]
+    boxBArea = boxB[2] * boxB[3]
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
+
+def non_max_suppression(boxes_scores_labels, overlapThresh):
+    if len(boxes_scores_labels) == 0:
+        return []
+
+    boxes_scores_labels.sort(key=lambda x: x[2], reverse=True)
+
+    labels = [item[0] for item in boxes_scores_labels]
+    boxes = [item[1] for item in boxes_scores_labels]
+    scores = [item[2] for item in boxes_scores_labels]
+
+    np_boxes = np.array([[b[0], b[1], b[0] + b[2], b[1] + b[3]] for b in boxes])
+    pick_indices = [] 
+
+    x1 = np_boxes[:, 0]
+    y1 = np_boxes[:, 1]
+    x2 = np_boxes[:, 2]
+    y2 = np_boxes[:, 3]
+    area = (x2 - x1) * (y2 - y1) 
+
+    idxs = np.arange(len(scores))
+
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick_indices.append(i)
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        w = np.maximum(0, xx2 - xx1)
+        h = np.maximum(0, yy2 - yy1)
+
+        intersection = w * h
+        union = area[i] + area[idxs[:last]] - intersection
+        overlap = intersection / union
+
+        idxs_to_delete = np.concatenate(([last], np.where(overlap > overlapThresh)[0]))
+        idxs = np.delete(idxs, idxs_to_delete)
+
+    final_detections = [(labels[i], boxes[i]) for i in pick_indices]
+    return final_detections
+    
 def setup_logger(log_path="log.txt"):
     log_dir = os.path.dirname(log_path)
     if log_dir and not os.path.exists(log_dir):
